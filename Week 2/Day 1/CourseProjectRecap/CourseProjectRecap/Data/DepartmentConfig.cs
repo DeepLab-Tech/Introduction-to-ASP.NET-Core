a1182d1618511b8d23585b4ca6689bec76994c9d
ozturksefa <ozturkksefa@gmail.com>
2021-03-23 14:49:39 +0300

    Added Week 2

---
+++ Week 2/Day 1/CourseProjectRecap/CourseProjectRecap/Data/DepartmentConfig.cs
@@ -0,0 +1,22 @@
+﻿using CourseProjectRecap.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CourseProjectRecap.Data
+{
+    public class DepartmentConfig : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.HasKey(k => k.DepartmentId);
+            builder.Property(p => p.DepartmentId).ValueGeneratedOnAdd();
+            builder.Property(p => p.DepartmentName).IsRequired().HasColumnType("Nvarchar(50)");
+            builder.Property(p => p.Budget).IsRequired();
+
+
+            builder.HasMany(m => m.Courses)
+                .WithOne(o => o.Department)
+                .HasForeignKey(f => f.DepartmentId);
+        }
+    }
+}
---
+++ Week 2/Day 1/CourseProjectRecap/CourseProjectRecap/Startup.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseProjectRecap.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace CourseProjectRecap
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=home}/{action=index}");
+            });
+
+            DbInitializer.Seed(app);
+        }
+    }
+}
