af189e59c48a359b0b4d50edb861d8d90c73e86e
ozturksefa <ozturkksefa@gmail.com>
2021-03-28 14:40:46 +0300

    update week 2

---
+++ Week 2/Day 2/CourseProjectRecap/CourseProjectRecap/Controllers/CourseController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseProjectRecap.Data;
+using CourseProjectRecap.Models;
+using CourseProjectRecap.Services.IRepository;
+
+namespace CourseProjectRecap.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseRepository _courseRepository;
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public CourseController(ApplicationDbContext context, ICourseRepository courseRepository, IDepartmentRepository departmentRepository)
+        {
+            _courseRepository = courseRepository;
+            _departmentRepository = departmentRepository;
+        }
+
+        public IActionResult Index()
+        {
+            //var tumKurslar = _context.Courses.ToList();
+
+            //var sorgu = from dept in _context.Department
+            //    join course in _context.Courses on dept.DepartmentId equals course.DepartmentId
+            //    select course;
+
+            var tumkurs = _courseRepository.CoursesToDepartment();
+
+
+
+
+            return View(tumkurs);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Departments = _departmentRepository.GetAll();
+            return View();
+        }
+
+        [HttpPost, ActionName("Create")]
+        public IActionResult CreatePost(Course model)
+        {
+            if (ModelState.IsValid)
+            {
+                _courseRepository.Add(model);
+                return RedirectToAction("Index");
+            }
+
+            return View("Create");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var kurs = _courseRepository.GetById(id);
+            if (kurs==null)
+            {
+                // Siz olsanız ne yaparsınız.
+                return NotFound();
+            }
+
+            ViewBag.Department = _departmentRepository.GetAll();
+            return View(kurs);
+        }
+
+    }
+}
---
+++ Week 2/Day 2/CourseProjectRecap/CourseProjectRecap/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CourseProjectRecap.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public decimal Budget { get; set; }
+
+        public ICollection<Course> Courses { get; set; }
+
+    }
+}
