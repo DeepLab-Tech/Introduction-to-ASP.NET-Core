4b5b28c147e21cdfe02ed279f1fed415fab95f06
ozturksefa <ozturkksefa@gmail.com>
2021-04-05 17:43:52 +0300

    updated Week 3 % added Day 3

---
+++ Week 3/Day 3/CourseProjectNTier/Models/Department.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Models
+{
+    public class Department : Base.Base
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public ICollection<Course> Courses { get; set; }
+        public ICollection<Teacher> Teachers { get; set; }
+    }
+}
